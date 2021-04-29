f4196cd0ddedb67079e63c41a8552f83339d0e43
KonTroAl <troshenkin.k@yandex.ru>
2021-04-29 12:14:58 +0500

    task 2 done

--- Homework_1/2.py
+++ Homework_1/2.py
@@ -1,2 +1,10 @@
 # 2. Каждое из слов «class», «function», «method» записать в байтовом типе без преобразования в последовательность кодов
 # (не используя методы encode и decode) и определить тип, содержимое и длину соответствующих переменных.
+
+val_1 = b'class'
+val_2 = b'function'
+val_3 = b'method'
+
+print(type(val_1), val_1, len(val_1))
+print(type(val_2), val_2, len(val_2))
+print(type(val_3), val_3, len(val_3))
