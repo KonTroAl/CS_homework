ae98e387accce314761447444720ee062981ab31
KonTroAl <troshenkin.k@yandex.ru>
2021-04-29 12:37:05 +0500

    task 6 done

--- Homework_1/6.py
+++ Homework_1/6.py
@@ -1,2 +1,11 @@
 # 6. Создать текстовый файл test_file.txt, заполнить его тремя строками: «сетевое программирование», «сокет», «декоратор».
 # Проверить кодировку файла по умолчанию. Принудительно открыть файл в формате Unicode и вывести его содержимое.
+
+with open('test_file.txt', 'r') as file:
+    print(file)
+
+# кодировка по умолчанию cp1251
+
+with open('test_file.txt', 'r', encoding='utf-8') as file:
+    for string in file:
+        print(string)
