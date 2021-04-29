f527ed4458ae8d42c9e45d3b8fb37bbafb0482e6
KonTroAl <troshenkin.k@yandex.ru>
2021-04-29 12:11:56 +0500

    update task 1

--- Homework_1/1.py
+++ Homework_1/1.py
@@ -9,9 +9,9 @@ print(val_1, type(val_1))
 print(val_2, type(val_2))
 print(val_3, type(val_3))
 
-val_1_unicode = b'\xd1\x80\xd0\xb0\xd0\xb7\xd1\x80\xd0\xb0\xd0\xb1\xd0\xbe\xd1\x82\xd0\xba\xd0\xb0'
-val_2_unicode = b'\xd1\x81\xd0\xbe\xd0\xba\xd0\xb5\xd1\x82'
-val_3_unicode = b'\xd0\xb4\xd0\xb5\xd0\xba\xd0\xbe\xd1\x80\xd0\xb0\xd1\x82\xd0\xbe\xd1\x80'
+val_1_unicode = '\u0440\u0430\u0437\u0440\u0430\u0431\u043e\u0442\u043a\u0430'
+val_2_unicode = '\u0441\u043e\u043a\u0435\u0442'
+val_3_unicode = '\u0434\u0435\u043a\u043e\u0440\u0430\u0442\u043e\u0440'
 
 print(val_1_unicode, type(val_1_unicode))
 print(val_2_unicode, type(val_2_unicode))
