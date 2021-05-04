f97de21450fe16ce78f6b1e769493091c944a51b
KonTroAl <troshenkin.k@yandex.ru>
2021-05-04 22:06:47 +0500

    authenticate completed

--- big_task/my_client.py
+++ big_task/my_client.py
@@ -7,3 +7,30 @@
 #         * addr — ip-адрес сервера;
 #         * port — tcp-порт на сервере, по умолчанию 7777.
 
+from socket import *
+import datetime, time
+import pickle
+
+timestamp = int(time.time())
+s = socket(AF_INET, SOCK_STREAM)
+s.connect(('localhost', 8007))
+
+users = {
+    'KonTroAll': 'SpaceShip007'
+}
+
+authenticate = True
+
+if authenticate:
+    dict_auth = {
+        'action': 'authenticate',
+        'time': timestamp,
+        'user': {
+            'user_name': 'KonTroAll',
+            'password': users['KonTroAll']
+        }
+    }
+    s.send(pickle.dumps(dict_auth))
+    data = s.recv(1024)
+    print('Сообщение от сервера: ', pickle.loads(data), ', длиной ', len(data), ' байт')
+    s.close()
--- big_task/my_server.py
+++ big_task/my_server.py
@@ -5,3 +5,47 @@
 #     - имеет параметры командной строки:
 #         * -p <port> — TCP-порт для работы (по умолчанию использует 7777);
 #         * -a <addr> — IP-адрес для прослушивания (по умолчанию слушает все доступные адреса).
+
+
+from socket import *
+import time
+import pickle
+
+s = socket(AF_INET, SOCK_STREAM)
+s.bind(('', 8007))
+s.listen(5)
+
+users = {
+    'KonTroAll': 'SpaceShip007'
+}
+
+
+while True:
+    client, addr = s.accept()
+    data = client.recv(1024)
+    client_data = pickle.loads(data)
+
+    if client_data['action'] == 'authenticate':
+        user = client_data['user']
+        for us, pas in users.items():
+            for val in user.values():
+                if us == val and pas == user['password']:
+                    dict_auth_response = {
+                        'response': 200,
+                        'alert': 'authenticate completed!',
+                        'action': 'quit'
+                    }
+                    print('authenticate completed!')
+                    client.send(pickle.dumps(dict_auth_response))
+                    break
+                else:
+                    dict_auth_response = {
+                        'response': 402,
+                        'error': 'This could be "wrong password" or "no account with that name"',
+                        'action': 'quit'
+                    }
+                    print('error!')
+                    client.send(pickle.dumps(dict_auth_response))
+                    break
+    client.close()
+
